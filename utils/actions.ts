c384cfd51f79e0a17c76e4094bbbb2f7fd24a7d6
Aditya Patidar <adityapatidar.2020@gmail.com>
2024-11-13 23:55:52 +0530

    create two actions fetchFeaturedProducts and fetchAllProducts

---
+++ utils/actions.ts
@@ -0,0 +1,19 @@
+import db from "@/utils/db";
+
+export async function fetchFeaturedProducts() {
+  const products = await db.product.findMany({
+    where: {
+      featured: true,
+    },
+  });
+  return products;
+}
+
+export async function fetchAllProducts() {
+  const products = await db.product.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+  return products;
+}
