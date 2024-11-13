27b98c5f42235576ea52c9f8ac05b89d7f11ba57
Aditya Patidar <adityapatidar.2020@gmail.com>
2024-11-14 00:19:52 +0530

    change the para of the carousel

--- app/_components/home/Hero.tsx
+++ app/_components/home/Hero.tsx
@@ -10,10 +10,10 @@ function Hero() {
           We are changing the way people shop
         </h1>
         <p className="mt-8 max-w-xl text-lg leading-8 text-muted-foreground">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque et
-          voluptas saepe in quae voluptate, laborum maiores possimus illum
-          reprehenderit aut delectus veniam cum perferendis unde sint doloremque
-          non nam.
+          Shop our curated selection of quality products across categories like
+          tech, fashion, and home essentials. Enjoy a seamless experience with
+          secure payments, fast shipping, and trusted brands, all designed for
+          your satisfaction.
         </p>
         <Button asChild size="lg" className="mt-10">
           <Link href="/products">Our Products</Link>
