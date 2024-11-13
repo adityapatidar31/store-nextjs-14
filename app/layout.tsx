0a1c95a819e82147b33adbb0e225cc8605106651
Aditya Patidar <adityapatidar.2020@gmail.com>
2024-11-13 20:11:22 +0530

    add navbar to websit and add container to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,10 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
+import { Inter } from "next/font/google";
+import Navbar from "./_components/navbar/Navbar";
+import Container from "./_components/global/Container";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const infer = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Next Store",
@@ -25,10 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={infer.className}>
+        <Navbar />
+        <Container className="py-20">{children}</Container>
       </body>
     </html>
   );
