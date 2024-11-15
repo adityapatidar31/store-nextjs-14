ea8e128dcbfee491f0b22d00f458eadd6f5dee10
Aditya Patidar <adityapatidar.2020@gmail.com>
2024-11-16 03:42:31 +0530

    add server action to fetch admin product

--- utils/actions.ts
+++ utils/actions.ts
@@ -14,6 +14,12 @@ const getAuthUser = async () => {
   return user;
 };
 
+const getAdminUser = async () => {
+  const user = await getAuthUser();
+  if (user.id !== process.env.ADMIN_USER_ID) redirect("/");
+  return user;
+};
+
 const renderError = (error: unknown): { message: string } => {
   console.log(error);
   return {
@@ -78,4 +84,15 @@ export const createProductAction = async (
     return renderError(error);
   }
   return redirect("/admin/products");
+};
+
+export const fetchAdminProducts = async () => {
+  await getAdminUser();
+  const products = await db.product.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  return products;
 };
