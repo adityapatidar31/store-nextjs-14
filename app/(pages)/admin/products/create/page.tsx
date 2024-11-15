15017c654c8de7451deb166c7ef7fbecad500142
Aditya Patidar <adityapatidar.2020@gmail.com>
2024-11-16 00:59:41 +0530

    add priceInput reusable comonnent in the create product form

--- app/(pages)/admin/products/create/page.tsx
+++ app/(pages)/admin/products/create/page.tsx
@@ -1,5 +1,3 @@
-import { Label } from "@/components/ui/label";
-import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { faker } from "@faker-js/faker";
 import FormInput from "@/app/_components/form/FormInput";
