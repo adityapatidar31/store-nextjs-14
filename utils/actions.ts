605218b5cc233a53c77d0bcab59eccaca8e61772
Aditya Patidar <adityapatidar.2020@gmail.com>
2024-11-16 02:17:02 +0530

    create azod schema for the productSchema

--- utils/actions.ts
+++ utils/actions.ts
@@ -54,8 +54,32 @@ export async function fetchSingleProduct(productId: string) {
 }
 
 export const createProductAction = async (
-  prevState: null,
+  prevState: any,
   formData: FormData
 ): Promise<{ message: string }> => {
-  return { message: "product created" };
+  const user = await getAuthUser();
+
+  try {
+    const name = formData.get("name") as string;
+    const company = formData.get("company") as string;
+    const price = Number(formData.get("price") as string);
+    const image = formData.get("image") as File;
+    const description = formData.get("description") as string;
+    const featured = Boolean(formData.get("featured") as string);
+
+    await db.product.create({
+      data: {
+        name,
+        company,
+        price,
+        image: "/images/product-1.jpg",
+        description,
+        featured,
+        clerkId: user.id,
+      },
+    });
+    return { message: "product created" };
+  } catch (error) {
+    return renderError(error);
+  }
 };
