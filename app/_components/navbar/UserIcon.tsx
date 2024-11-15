feec1b19e7bb0ae198ad076664121322e338f536
Aditya Patidar <adityapatidar.2020@gmail.com>
2024-11-15 21:32:45 +0530

    complete the UserIcon.tsx for the user icon on the app for the login user as well as logout user

--- app/_components/navbar/UserIcon.tsx
+++ app/_components/navbar/UserIcon.tsx
@@ -1,7 +1,23 @@
+import { currentUser } from "@clerk/nextjs/server";
+import Image from "next/image";
 import React from "react";
+import { LuUser2 } from "react-icons/lu";
 
-function UserIcon() {
-  return <div>UserIcon</div>;
+async function UserIcon() {
+  const user = await currentUser();
+  const profileImage = user?.imageUrl;
+  if (profileImage) {
+    return (
+      <Image
+        src={profileImage}
+        alt="User Profile"
+        width={24}
+        height={24}
+        className="rounded-full object-cover"
+      />
+    );
+  }
+  return <LuUser2 className="w-6 h-6 rounded-full text-white" />;
 }
 
 export default UserIcon;
