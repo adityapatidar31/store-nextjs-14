7e9339f9770b3a9d1b8fe1f70648e51241523da0
Aditya Patidar <adityapatidar.2020@gmail.com>
2024-11-15 21:22:35 +0530

    complete the logout button with toast

--- app/_components/navbar/SignOutLink.tsx
+++ app/_components/navbar/SignOutLink.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+"use client";
+
+import { useToast } from "@/hooks/use-toast";
+import { SignOutButton } from "@clerk/nextjs";
+import Link from "next/link";
 
 function SignOutLink() {
-  return <div>signOutLink</div>;
+  const { toast } = useToast();
+  function handleLogout() {
+    toast({ description: "Logout Successful" });
+  }
+  return (
+    <SignOutButton>
+      <Link href="/" className="w-full text-left" onClick={handleLogout}></Link>
+    </SignOutButton>
+  );
 }
 
 export default SignOutLink;
