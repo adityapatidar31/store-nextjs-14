662cde4cacd720f8697d5c76d8fd16dfdeadefc4
Aditya Patidar <adityapatidar.2020@gmail.com>
2024-11-16 02:05:45 +0530

    create two helper function 1. to check user is sign in our not if not the redirect to home and 2. error message handeller in which we return appro. error according to error.message

--- utils/actions.ts
+++ utils/actions.ts
@@ -2,6 +2,22 @@
 
 import db from "@/utils/db";
 import { redirect } from "next/navigation";
+import { currentUser } from "@clerk/nextjs/server";
+
+const getAuthUser = async () => {
+  const user = await currentUser();
+  if (!user) {
+    throw new Error("You must be logged in to access this route");
+  }
+  return user;
+};
+
+const renderError = (error: unknown): { message: string } => {
+  console.log(error);
+  return {
+    message: error instanceof Error ? error.message : "An error occurred",
+  };
+};
 
 export async function fetchFeaturedProducts() {
   const products = await db.product.findMany({
