d52bbc53c1e977b2caff92fea2a8bdf4852c96f3
Aditya Patidar <adityapatidar.2020@gmail.com>
2024-11-18 21:25:10 +0530

    update the singleProductPage if user created the review then it can not create again

--- app/(pages)/products/[id]/page.tsx
+++ app/(pages)/products/[id]/page.tsx
@@ -1,22 +1,22 @@
-import { fetchSingleProduct } from "@/utils/actions";
+import { fetchSingleProduct, findExistingReview } from "@/utils/actions";
 import Image from "next/image";
 import { formatCurrency } from "@/utils/format";
+import { auth } from "@clerk/nextjs/server";
 import BreadCrumbs from "@/app/_components/single-product/BreadCrumbs";
 import FavoriteToggleButton from "@/app/_components/products/FavoriteToggleButton";
+import ShareButton from "@/app/_components/single-product/ShareButton";
 import ProductRating from "@/app/_components/single-product/ProductRating";
 import AddToCart from "@/app/_components/single-product/AddToCart";
-import ShareButton from "@/app/_components/single-product/ShareButton";
+import ProductReviews from "@/app/_components/reviews/ProductReview";
 import SubmitReview from "@/app/_components/reviews/SubmitReview";
-import ProductReview from "@/app/_components/reviews/ProductReview";
 
-type PropType = {
-  params: { id: string };
-};
-
-async function SingleProductPage({ params }: PropType) {
+async function SingleProductPage({ params }: { params: { id: string } }) {
   const product = await fetchSingleProduct(params.id);
   const { name, image, company, description, price } = product;
   const dollarsAmount = formatCurrency(price);
+  const { userId } = auth();
+  const reviewDoesNotExist =
+    userId && !(await findExistingReview(userId, product.id));
   return (
     <section>
       <BreadCrumbs name={product.name} />
@@ -27,15 +27,15 @@ async function SingleProductPage({ params }: PropType) {
             src={image}
             alt={name}
             fill
-            sizes="(max-width:768px) 100vw,(max-width:1200px) 50vw,33vw"
+            sizes="(max-width:768px) 100vw,(max-width:1200px) 50vw, 33vw"
             priority
-            className="w-full rounded-md object-cover"
+            className="w-full rounded object-cover"
           />
         </div>
         {/* PRODUCT INFO SECOND COL */}
         <div>
           <div className="flex gap-x-8 items-center">
-            <h1 className="capitalize text-3xl font-bold">{name}</h1>
+            <h1 className="capitalize text-3xl font-bold">{name} </h1>
             <div className="flex items-center gap-x-2">
               <FavoriteToggleButton productId={params.id} />
               <ShareButton name={product.name} productId={params.id} />
@@ -43,13 +43,16 @@ async function SingleProductPage({ params }: PropType) {
           </div>
           <ProductRating productId={params.id} />
           <h4 className="text-xl mt-2">{company}</h4>
-          <p className="mt-3 text-md bg-muted inline-block p-2 rounded-md">
+          <p className="mt-3 text-md bg-muted inline-block p-2 rounded">
             {dollarsAmount}
           </p>
           <p className="mt-6 leading-8 text-muted-foreground">{description}</p>
           <AddToCart productId={params.id} />
         </div>
       </div>
+      <ProductReviews productId={params.id} />
+
+      {reviewDoesNotExist && <SubmitReview productId={params.id} />}
     </section>
   );
 }
