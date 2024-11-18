28514d659fa2403dea6f1063437542b232faed73
Aditya Patidar <adityapatidar.2020@gmail.com>
2024-11-18 23:07:01 +0530

    create placeholder functions for all the feature we need for the card section

--- utils/actions.ts
+++ utils/actions.ts
@@ -332,3 +332,19 @@ export const createReviewAction = async (
     return renderError(error);
   }
 };
+
+export const fetchCartItems = async () => {};
+
+export const fetchProduct = async () => {};
+
+export const fetchOrCreateCart = async () => {};
+
+export const updateOrCreateCartItem = async () => {};
+
+export const updateCart = async () => {};
+
+export const addToCartAction = async () => {};
+
+export const removeCartItemAction = async () => {};
+
+export const updateCartItemAction = async () => {};
