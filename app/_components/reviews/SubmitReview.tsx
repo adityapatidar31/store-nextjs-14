a9ddfb4da9580c14df03666707fa796535c5318e
Aditya Patidar <adityapatidar.2020@gmail.com>
2024-11-18 19:09:45 +0530

    create submit review form just createReviewAction is not done yet

---
+++ app/_components/reviews/SubmitReview.tsx
@@ -0,0 +1,51 @@
+"use client";
+import { useState } from "react";
+import { Card } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { createReviewAction } from "@/utils/actions";
+import { useUser } from "@clerk/nextjs";
+import FormContainer from "../form/FormContainer";
+import RatingInput from "./RatingInput";
+import TextAreaInput from "../form/TextAreaInput";
+import { SubmitButton } from "../form/Buttons";
+function SubmitReview({ productId }: { productId: string }) {
+  const [isReviewFormVisible, setIsReviewFormVisible] = useState(false);
+  const { user } = useUser();
+  return (
+    <div>
+      <Button
+        size="lg"
+        className="capitalize"
+        onClick={() => setIsReviewFormVisible((prev) => !prev)}
+      >
+        leave review
+      </Button>
+      {isReviewFormVisible && (
+        <Card className="p-8 mt-8">
+          <FormContainer action={createReviewAction}>
+            <input type="hidden" name="productId" value={productId} />
+            <input
+              type="hidden"
+              name="authorName"
+              value={user?.firstName || "user"}
+            />
+            <input
+              type="hidden"
+              name="authorImageUrl"
+              value={user?.imageUrl || ""}
+            />
+            <RatingInput name="rating" />
+            <TextAreaInput
+              name="comment"
+              labelText="feedback"
+              defaultValue="Outstanding product!!!"
+            />
+            <SubmitButton className="mt-4" />
+          </FormContainer>
+        </Card>
+      )}
+    </div>
+  );
+}
+
+export default SubmitReview;
