523ea76b8bb45bf433e23f45273ba596247785b9
Aditya Patidar <adityapatidar.2020@gmail.com>
2024-11-19 22:18:15 +0530

    create a first column of the cartItemColumns

--- app/_components/cart/CartItemColumns.tsx
+++ app/_components/cart/CartItemColumns.tsx
@@ -1,7 +1,21 @@
-import React from "react";
-
-function CartItemColumns() {
-  return <div></div>;
-}
-
-export default CartItemColumns;
+import Image from "next/image";
+export const FirstColumn = ({
+  name,
+  image,
+}: {
+  image: string;
+  name: string;
+}) => {
+  return (
+    <div className="relative h-24 w-24 sm:h-32 sm:w-32">
+      <Image
+        src={image}
+        alt={name}
+        fill
+        sizes="(max-width:768px) 100vw,(max-width:1200px) 50vw,33vw"
+        priority
+        className="w-full rounded-md object-cover"
+      />
+    </div>
+  );
+};
