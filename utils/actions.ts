57f68abd4f51ee6dad9f8a3b41f1da9d680c594d
Aditya Patidar <adityapatidar.2020@gmail.com>
2024-11-19 22:41:36 +0530

    complete the updateCartItemAction for the update the quantity of the product

--- utils/actions.ts
+++ utils/actions.ts
@@ -512,4 +512,33 @@ export const removeCartItemAction = async (
   }
 };
 
-export const updateCartItemAction = async () => {};
+export const updateCartItemAction = async ({
+  amount,
+  cartItemId,
+}: {
+  amount: number;
+  cartItemId: string;
+}) => {
+  const user = await getAuthUser();
+
+  try {
+    const cart = await fetchOrCreateCart({
+      userId: user.id,
+      errorOnFailure: true,
+    });
+    await db.cartItem.update({
+      where: {
+        id: cartItemId,
+        cartId: cart.id,
+      },
+      data: {
+        amount,
+      },
+    });
+    await updateCart(cart);
+    revalidatePath("/cart");
+    return { message: "cart updated" };
+  } catch (error) {
+    return renderError(error);
+  }
+};
